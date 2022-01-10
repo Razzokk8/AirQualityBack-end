9a222917f7621516d06f464c76be08b6c258247d
Riccardo Limiti <riccardo.limiti@gmail.com>
2022-01-10 15:57:50 +0100

    finaL

--- README.md
+++ README.md
@@ -4,3 +4,28 @@
 
 1. Create a `.env` file from a copy of the `.env.example` file
 2. Fill in the details accordingly
+
+IDEA:
+
+AIR POLLUTION API x Bikers
+
+The idea is to create an air pollution monitoring service for people use bikes.
+this service is aimed at a "technological" users target able to detect data and send them to the database with specialized equipment such as Raspberry Pi DIY Air Quality Monitor or Arduino Meter.
+
+1. User Schema
+
+2. Air Conditions Schema
+
+* Backend service *
+
+A user would like to create an account-
+
+"/user/register"
+
+A user would like to submit data about the air quality in their area-
+
+"/air/submit"
+
+A user would like to read historical air quality data in their area-
+
+"/air/view/:date/:city"
--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const User = require('../models/User')
-const router = express.Router()
 const bcrypt = require('bcrypt')
+const router = express.Router()
 
 router.post("/create", async (req, res) => {
-    const {body} = req;
+    // const {body} = req;
+    const {passWord, firstName, lastName, email} = req.body;
   try {
-    const hashedPass = await bcrypt.hash(req.body.password, 10)
+    const hashedPassword = await bcrypt.hash(passWord, 10)
     const users = await User.create({
 
-    firstName: body.firstName,
-    lastName: body.lastName,
-    email: body.email,
-    passWord: hashedPass
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    passWord: hashedPassword
 
 });
     return res.status(200).json(users)
-
   } catch (error) {
     return res.status(500).send("Data not valid 🖕");
   }
